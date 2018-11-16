999880d1ff6c77e4f8b64e1edcb0f50462c2df3b
lucien1011 <lucien1011@gmail.com>
2018-11-16 11:04:01 -0500

    Migrate to class based framework

--- KinZfitter/make.sh
+++
@@ -1,2 +0,0 @@
-c++ -std=c++14 -O2 -Wall `root-config --cflags --ldflags --glibs` -L/cvmfs/cms.cern.ch/sl6_amd64_gcc530/lcg/root/6.06.00-ikhhed4//lib -lRooFit -lRooFitCore -lRooStats  -c KinZfitter.cpp
-c++ -std=c++14 -O2 -Wall `root-config --cflags --ldflags --glibs` -L/cvmfs/cms.cern.ch/sl6_amd64_gcc530/lcg/root/6.06.00-ikhhed4//lib -lRooFit -lRooFitCore -lRooStats  -c HelperFunction.cc
