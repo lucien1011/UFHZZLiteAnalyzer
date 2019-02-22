bcbd912f1aa1406f01efe077db43effca26d1c3a
lucien1011 <lucien1011@gmail.com>
2019-02-22 05:53:53 -0500

    Tidy up 80X

--- run_LiteDarkZTree_80XData.py
+++ run_LiteDarkZTree_80XData.py
@@ -1,11 +1,12 @@
 import ROOT,os
 from PyUtils.UFTier2 import t2_prefix
 from PyUtils.Shell import makedirs
+from Emailer.Utils import sendQuickMail
 
 # ____________________________________________________________________________________________________________________________________ ||
-inputDir        = t2_prefix+'/store/user/t2/users/archived/dsperka/Run2/HZZ4l/SubmitArea_13TeV/rootfiles_Data80X_4lskim_M17_Feb21/'
+inputDir        = t2_prefix+'/store/user/t2/users/klo/Higgs/HZZ4l/NTuple/Run2/Data_80XM17_FebCombined/'
 inputTreeName   = "passedEvents"
-outputDir       = "/raid/raid7/lucien/Higgs/DarkZ-NTuple/20181116/SkimTree_DarkPhoton_Run2016Data_m4l70/"
+outputDir       = "/raid/raid7/lucien/Higgs/DarkZ-NTuple/20190122/SkimTree_DarkPhoton_Run2016Data_m4l70/"
 
 fileNames = [
     "Data_Run2016-03Feb2017_4l.root",
@@ -18,5 +19,13 @@ makedirs(outputDir)
 for fileName in fileNames:
     ana = ROOT.LiteHZZTreeProducer(outputDir,fileName)
     ana.loop(inputDir+fileName,inputTreeName)
-
-
+sendQuickMail(
+            ["klo@cern.ch",],
+            "UFHZZLiteAnalyzer finished processing ("+emailer.getTimeStr()+") ",
+            "\n".join([
+                "Input directory: "+baseInputDir,
+                "Output directory: "+outputDir,
+                "File: "+",".join(fileNames),
+                ]
+                ),
+            )
