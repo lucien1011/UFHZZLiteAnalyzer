bfc1138c1e1533fbd257d6e7ec7b64552f834ba5
lucien1011 <lucien1011@gmail.com>
2020-06-13 18:55:39 -0400

    Update

---
+++ run_LiteDarkZTree_80XMC_HZZdInterpolation.py
@@ -0,0 +1,38 @@
+import ROOT,os
+from PyUtils.UFTier2 import t2_prefix
+from PyUtils.Shell import makedirs
+
+# ____________________________________________________________________________________________________________________________________ ||
+inputDir        = t2_prefix+'/store/user/t2/users/klo/Higgs/HToZZd/80X_MCProd_191127/'
+inputTreeName   = "Ana/passedEvents"
+outputDir       = "/raid/raid7/lucien/Higgs/DarkZ-NTuple/20191204/SkimTree_DarkPhoton_Run2016Data_m4l70/"
+
+fileNames = [
+        "HToZZdTo4L_M125_MZd3_eps1e-2_13TeV_madgraph_pythia8.root",
+        "HToZZdTo4L_M125_MZd2_eps1e-2_13TeV_madgraph_pythia8.root",   
+        "HToZZdTo4L_M125_MZd4_eps1e-2_13TeV_madgraph_pythia8.root",
+        "HToZZdTo4L_M125_MZd1_eps1e-2_13TeV_madgraph_pythia8.root",   
+        ]
+
+
+# ____________________________________________________________________________________________________________________________________ ||
+ROOT.gSystem.Load("include/LiteHZZTreeProducer_h.so")
+
+makedirs(outputDir)
+for fileName in fileNames:
+    ana = ROOT.LiteHZZTreeProducer(
+            9999999.,
+            70.,
+            120.,
+            0.1,
+            120.,
+            40.,
+            999999.,
+            0.35,
+            outputDir,
+            fileName,
+            False,
+            True,
+            0.1,
+            )
+    ana.loop(inputDir+fileName,inputTreeName)
--- run_LiteDarkZTree_80XMC_HZdZd.py
+++ run_LiteDarkZTree_80XMC_HZdZd.py
@@ -9,20 +9,20 @@ inputTreeName   = "Ana/passedEvents"
 outputDir       = "/raid/raid7/lucien/Higgs/DarkZ-NTuple/20191120/SkimTree_DarkPhoton_Run2016Data_m4l70/"
 
 fileNames = [
-        #"GluGluHToZZTo4L_M125_13TeV_powheg2_minloHJ_NNLOPS_JHUgenV702_pythia8.root", 
+        "GluGluHToZZTo4L_M125_13TeV_powheg2_minloHJ_NNLOPS_JHUgenV702_pythia8.root", 
         "GluGluHToZZTo4L_M125_13TeV_powheg2_JHUgenV6_pythia8.root",
-        #"ZZTo4L_13TeV_powheg_pythia8.root",
-        #"VBF_HToZZTo4L_M125_13TeV_powheg2_JHUgenV6_pythia8.root",
-        #"WplusH_HToZZTo4L_M125_13TeV_powheg2-minlo-HWJ_JHUgenV6_pythia8.root",
-        #"WminusH_HToZZTo4L_M125_13TeV_powheg2-minlo-HWJ_JHUgenV6_pythia8.root",
-        #"ZH_HToZZ_4LFilter_M125_13TeV_powheg2-minlo-HZJ_JHUgenV6_pythia8.root",
-        #"GluGluToContinToZZTo4e_13TeV_MCFM701_pythia8.root",       
-        #"GluGluToContinToZZTo2mu2tau_13TeV_MCFM701_pythia8.root",   
-        #"GluGluToContinToZZTo4mu_13TeV_MCFM701_pythia8.root",
-        #"GluGluToContinToZZTo2e2mu_13TeV_MCFM701_pythia8.root",
-        #"GluGluToContinToZZTo4tau_13TeV_MCFM701_pythia8.root",   
-        #"GluGluToContinToZZTo2e2tau_13TeV_MCFM701_pythia8.root",     
-        #"ttH_HToZZ_4LFilter_M125_13TeV_powheg2_JHUgenV6_pythia8.root",   
+        "ZZTo4L_13TeV_powheg_pythia8.root",
+        "VBF_HToZZTo4L_M125_13TeV_powheg2_JHUgenV6_pythia8.root",
+        "WplusH_HToZZTo4L_M125_13TeV_powheg2-minlo-HWJ_JHUgenV6_pythia8.root",
+        "WminusH_HToZZTo4L_M125_13TeV_powheg2-minlo-HWJ_JHUgenV6_pythia8.root",
+        "ZH_HToZZ_4LFilter_M125_13TeV_powheg2-minlo-HZJ_JHUgenV6_pythia8.root",
+        "GluGluToContinToZZTo4e_13TeV_MCFM701_pythia8.root",       
+        "GluGluToContinToZZTo2mu2tau_13TeV_MCFM701_pythia8.root",   
+        "GluGluToContinToZZTo4mu_13TeV_MCFM701_pythia8.root",
+        "GluGluToContinToZZTo2e2mu_13TeV_MCFM701_pythia8.root",
+        "GluGluToContinToZZTo4tau_13TeV_MCFM701_pythia8.root",   
+        "GluGluToContinToZZTo2e2tau_13TeV_MCFM701_pythia8.root",     
+        "ttH_HToZZ_4LFilter_M125_13TeV_powheg2_JHUgenV6_pythia8.root",   
         ]
 
 # ____________________________________________________________________________________________________________________________________ ||
